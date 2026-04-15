5ed0b9370e6f2f8863a7e0aa0d42705953beb32f
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-04-15 12:22:15 +0200

    exercices 1

---
+++ stagiaires/Bruce/01-exemple-connexion/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+#premiere connexion via PDO
+
+$connexionPDO = new PDO(
+    dsn: "mysql:host=localhost;dbname=listepays;port=3307;charset=utf8mb4",
+    username: "root",  
+    password: "" ,
+);
+$request = $connexionPDO->query("SELECT * FROM countries");
+var_dump($connexionPDO,$request);
+
+while ($item = $request->fetch(PDO::FETCH_ASSOC)) {
+    echo $item['nom']. " | " ;
+}
