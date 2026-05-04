e7c884a2dfae15e5b0471fde18abbee41973eb50
raphaeldebot <debot.raphael@gmail.com>
2026-05-04 12:00:30 +0200

    fin de l'ex 3

--- stagiaires/Raphael/03-livreor-query-exe/index.php
+++ stagiaires/Raphael/03-livreor-query-exe/index.php
@@ -9,8 +9,48 @@ try{
 }catch(Exception $e){
     // arrêt et affichage de l'erreur (en dev)
     die($e->getMessage());
+}
+
+// On vérifie si on a envoyé le formulaire
+if(isset($_POST['email_message'],$_POST['texte_message'])){
+
+    // On va Créer des variables de traitement :
+
+    # Retourne le mail (string) si valide via expression régulière, sinon false (bool)
+    $email = filter_var($_POST['email_message'], FILTER_VALIDATE_EMAIL);
+
+    # On retire les balise html pour sécuriser la chaîne attention (! très sécure seulement
+    # si on ne permet aucune balise, l'htmlspecialchars est hautement recommandé)
+    $text = strip_tags($_POST['texte_message']);
+
+    # On retire les espaces vides devant et derrière la chaîne
+    $text = trim($text);
+
+    # On convertit les caractères spéciaux dangereux pour injection SQL et/ou XSS
+    # en entité html
+    $text = htmlspecialchars($text);
+
+    #echo nl2br($text);
+
+    #var_dump($email,$text);
+
+    // Si le mail ne vaut pas false ET que le texte n'est pas vide
+    if ($email!==false &&  $text!==""){
+        // préparation de la requète (pour s'habituer ;-)
+        $sql = "INSERT INTO `message` (`email_message`,`texte_message`)
+        VALUE ('$email','$text');";
+        // Execution de l'insertion qui contiendra le nombre
+        // de ligne affectées pas la requête
+        $nb_affected_line = $connectDB->exec($sql);
+        
+        // si au moins une ligne est affectée (1 == true, 0 == false)
+        if($nb_affected_line)
+            $thanks = "Merci pour l'ajout";
+
+    }
 
 }
+
 
 // On récupère les messages
 $request = $connectDB->query("SELECT * FROM `message`");
@@ -44,20 +84,51 @@ $connectDB = null;
 <body>
     <h1>Livre d'oir</h1>
     <p>Merci de me laisser un message sur l'événement ABC</p>
+    <?php
+    if(isset($thanks)):
+    ?>
+    <h3><?=$thanks?></h3>
+    <script>
+        setTimeout(() => {
+            window.location.href="./";
+        }
+            , "3000");
+    </script>
+    <?php
+    endif;
+    ?>
     <form action="" method="POST" name='Message'>
         <input type="text" name="email_message" placeholder="Votre mail" /><br>
         <textarea name="texte_message" placeholder="Votre message"></textarea><br>
         <input type="submit" value="Envoyer">
         <?php
-        var_dump($_POST);
+        // var_dump($_POST);
         ?>
     </form>
     <div>
     <?php
+    // Pas de messages postés
     if(isset($message)):
     ?>
     <h3><?= $message ?></h3>
     <?php
+    // On a au moins un message dans la table
+    else:
+        // Pour le pluriel
+        $pluriel = $nbMessage > 1 ? "s" : "";
+    ?>
+    <h3>Nous avons <?= $nbMessage ?> message<?=$pluriel?></h3>
+    <?php
+        // Tant qu'on a des messages
+        foreach($results as $result):
+    ?>
+    <div class="reponse">
+        <h5><?= $result['email_message'] ?> a écrit à <?= $result['date_message'] ?></h5>
+        <p><?= nl2br($result['texte_message']); // retour automatique à la ligne ?></p>
+        <hr>
+    </div>
+    <?php
+        endforeach;
     endif;
     ?>
     </div>
